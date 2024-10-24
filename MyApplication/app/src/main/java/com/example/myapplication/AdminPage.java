783a9d4851148df82d8a2aebf40e665435dcc611
mkamaUO <mkama022@uottawa.ca>
2024-10-24 15:02:19 -0400

    Added admin page banner and ensured button only shows up for admin

--- MyApplication/app/src/main/java/com/example/myapplication/AdminPage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/AdminPage.java
@@ -1,4 +1,35 @@
-package com.example.myapplication.data;
+package com.example.myapplication;
 
-public class AdminPage {
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AdminPage extends AppCompatActivity{
+    private TextView textView6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/WelcomePage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/WelcomePage.java
@@ -3,11 +3,8 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,9 +18,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,11 +25,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.ktx.Firebase;
 
 public class WelcomePage extends AppCompatActivity {
     private TextView nameView;
     private TextView roleView;
+    private Button adminButton;
     Spinner spinnerr;
 
     private FirebaseAuth myAuth;
@@ -58,6 +52,7 @@ public class WelcomePage extends AppCompatActivity {
 
             mUser = myAuth.getCurrentUser();
             nameView = findViewById(R.id.welcome);
+            adminButton = findViewById(R.id.adminPageButton);
             mDatabase = FirebaseDatabase.getInstance().getReference("users").child(mUser.getUid());
             mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
@@ -65,11 +60,22 @@ public class WelcomePage extends AppCompatActivity {
                     String names = snapshot.child("name").getValue(String.class);
                     String roles = snapshot.child("role").getValue(String.class);
                     nameView.setText("Welcome "+names+"! "+"You are logged in as a " + roles+".");
+                    if (roles != null && roles.equals("admin")) {
+                        adminButton.setVisibility(View.VISIBLE);
+                    }
                 }
-
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
                     Toast.makeText(WelcomePage.this, "Something Wrong Happened", Toast.LENGTH_LONG).show();
                 }
             });
-        }}
+            adminButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(WelcomePage.this, AdminPage.class));
+                }
+            });
+        }
+
+
+}
